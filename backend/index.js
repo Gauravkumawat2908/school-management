7d144359b6ee54758c819c43e21d5a03fd094024
Gauravkumawat2908 <gauravkumawat2908@gmail.com>
2025-08-16 05:42:01 +0530

    gitignore

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const Routes = require("./routes/route.js")
 
 
 // in backend/index.js (or server.js)
-const PORT = process.env.PORT || 2000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 dotenv.config();
